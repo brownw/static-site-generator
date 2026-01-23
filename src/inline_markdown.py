13e77975be2d3f161905821f7231d4e26f38af9b
brownw <brownw@gmail.com>
2026-01-22 23:23:16 -0500

    Ch3 L5

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -28,3 +28,67 @@ def extract_markdown_images(text):
     
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            raise Exception(f"{node} is not a TextNode")
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        original_text = node.text
+        images = extract_markdown_images(node.text)
+
+        if not images:
+            new_nodes.append(node)
+    
+        for image in images:
+            image_alt, image_url = image
+            sections = original_text.split(f"![{image_alt}]({image_url})", 1)
+            
+            if len(sections) != 2:
+                   raise ValueError("Invalid markdown, image section not found")
+            
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+            
+            new_nodes.append(TextNode(image_alt,TextType.IMAGE,image_url))
+            original_text = sections[1]
+        
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            raise Exception(f"{node} is not a TextNode")
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        original_text = node.text
+        links = extract_markdown_links(node.text)
+
+        if not links:
+            new_nodes.append(node)
+    
+        for link in links:
+            link_text, link_url = link
+            sections = original_text.split(f"[{link_text}]({link_url})", 1)
+            
+            if len(sections) != 2:
+                   raise ValueError("Invalid markdown, link section not found")
+            
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+            
+            new_nodes.append(TextNode(link_text,TextType.LINK,link_url))
+            original_text = sections[1]
+        
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -125,5 +125,42 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         )
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                   "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a [link](https://i.imgur.com/zjjcJKZ.png) and another [link](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                   "link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
