e3345cba28a97edb6fa0ffa67bd326d1cb30ec72
brownw <brownw@gmail.com>
2026-02-02 20:52:07 -0500

    in process

--- src/extract_title.py
+++ src/extract_title.py
@@ -16,16 +16,24 @@ def generate_page(from_path, template_path, dest_path):
         from_file = open(from_path)
         source = from_file.read()
         from_file.close() 
+        print(f"{source}")
         #Read the template file at template_path and store the contents in a variable.
         template_file = open(template_path)
         template = template_file.read()
         template_file.close()
+        print(f"{template}")
         #Use your markdown_to_html_node function and .to_html() method to convert the markdown file to an HTML string.
         html_node = markdown_to_html_node(source)
-        html_text = html_node.to_html()
+        #html_text = html_node.to_html()
+        print(f"html_text: {html_node.to_html()}")
         #Use the extract_title function to grab the title of the page.
-        title = extract_title(source)
-    except:
-        print("Something went wrong")
-    #Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML and title you generated.
-    #Write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist.
+        #title = extract_title(source)
+        #template.replace("{{Title}}", title).replace("{{Content}}", html_text)
+        #Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML and title you generated.
+        #Write the new full HTML page to a file at dest_path. Be sure to create any necessary directories if they don't exist.
+    except FileNotFoundError as e:
+        print(f"Sorry, the file does not exist: {e}")
+    except PermissionError as e:
+        print(f"You do not have permission to read this file. {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
